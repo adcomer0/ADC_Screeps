9d468dc1d95c315bf00f37ae681a5e9ab051b6ca
adcomer0 <adcomer0@gmail.com>
2017-04-11 12:02:06 -0500

    Update TowerController.js

--- src/TowerController.js
+++ src/TowerController.js
@@ -3,18 +3,25 @@ var MyTowers = {
         //defend room
         var maxRange = 25;
         var hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
+        var myCreeps = tower.room.find(FIND_MY_CREEPS, {
+            filter: function(object){
+                return object.hits < object.hitsMax;
+            }
+        });
         if (hostiles.length > 0) {
             var username = hostiles[0].owner.username;
             if (username !== 'Invader') {
                 Game.notify('User ' + username + ' spotted in room ' + tower.room.name, 1);
             }
             tower.attack(hostiles[0]);
+        } else if(myCreeps.length > 0) {
+            tower.heal(myCreeps[0]);
         }
         else if (tower.energy > tower.energyCapacity/2) {
             //ReinforceRamparts
             var ramparts = tower.pos.findInRange(FIND_STRUCTURES, maxRange, {
                 filter: function(object){
-                        return (object.structureType === STRUCTURE_RAMPART && object.hits < object.hitsMax)
+                        return (object.structureType === STRUCTURE_RAMPART && object.hits < object.hitsMax/2)
                     }
             });
             if (ramparts.length > 0) {
@@ -24,7 +31,7 @@ var MyTowers = {
                 //reinforceWalls
                 var walls = tower.pos.findInRange(FIND_STRUCTURES, maxRange, {
                     filter: function(object){
-                        return (object.structureType === STRUCTURE_WALL && object.hits < object.hitsMax)
+                        return (object.structureType === STRUCTURE_WALL && object.hits < 100000)
                     }
                 });
                 if (walls.length > 0) {
@@ -34,7 +41,7 @@ var MyTowers = {
                     //repairRoads
                     var roads = tower.pos.findInRange(FIND_STRUCTURES, maxRange, {
                         filter: function(object){
-                        return (object.structureType === STRUCTURE_ROAD && object.hits < object.hitsMax)
+                        return (object.structureType === STRUCTURE_ROAD && object.hits < object.hitsMax/3)
                     }
                     });
                     if (roads.length > 0) {
