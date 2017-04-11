80f95053a1ea62ecb7be018225ebdef7e5c0741c
adcomer0 <adcomer0@gmail.com>
2017-04-11 12:05:03 -0500

    Update role.upgrader.js

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -11,16 +11,33 @@ var roleUpgrader = {
 	    }
 	    
 	    if (creep.memory.harvesting){
-	        var room1 = 'W1N7';
+	        var room1 = 'W3N4';
 	        var room2 = 'W2N7';
             if (creep.room.name == room1){
-	            var target = creep.pos.findClosestByRange(FIND_SOURCES, {
+	            /*var target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY, {
 	                filter: function(object){
-	                    return (object.id === 'ef990774d80108c');
+	                    return (object.amount > 200);
 	                }
 	            });
-	        
-                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+	            
+                if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target,{visualizePathStyle: {
+                        fill: 'transparent', 
+                        stroke: '#fff', 
+                        lineStyle: 'dashed', 
+                        strokeWidth: .15, 
+                        opacity: .1}});
+                }*/
+                var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                    filter: function(object){
+                        return (object.structureType == STRUCTURE_STORAGE && object.store.energy > 800);
+                    }
+                });
+                
+                if (!target){
+                    creep.moveTo(Game.flags.Flag3)
+                }
+                if (creep.withdraw(target,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                     creep.moveTo(target,{visualizePathStyle: {
                         fill: 'transparent', 
                         stroke: '#fff', 
